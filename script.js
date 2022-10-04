4d031e8629185edd0e9d9e4831a0bc7cc4bf061b
Allan Medeiros Minato <allan.minato@gmail.com>
2022-10-04 16:24:36 -0300

    resolução do exercicio 1 e 2

---
+++ script.js
@@ -0,0 +1,49 @@
+
+    let color = document.getElementsByClassName('color')[0];
+    let color2 = document.getElementsByClassName('color')[1];
+    let color3 = document.getElementsByClassName('color')[2];
+    let color4 = document.getElementsByClassName('color')[3];
+
+
+    color.style.backgroundColor = "black";
+    color2.style.backgroundColor = "pink";
+    color3.style.backgroundColor = "red";
+    color4.style.backgroundColor = "brown";
+    
+    
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

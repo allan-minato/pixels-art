7ad307403c67d239e1eb050f1c6b9aeb263ff690
Allan Medeiros Minato <allan.minato@gmail.com>
2022-10-04 17:55:58 -0300

    resolução cores repetidas.

--- script.js
+++ script.js
@@ -9,12 +9,26 @@
     color2.style.backgroundColor = "pink";
     color3.style.backgroundColor = "red";
     color4.style.backgroundColor = "brown";
+
+    const colorPalette = document.querySelectorAll(".color");
     
+    function generateColors() {
+        colorPalette.forEach((color) =>{
+        let hexCode = "#" +  Math.random().toString(16).substring(2, 8);
+        color.style.backgroundColor = hexCode;    
+        
+        if(color === hexCode){     
+        generateColors();    
+        }    
+    })
+    let color = document.getElementsByClassName('color')[0];
+    color.style.backgroundColor = "black";
+    }
+    
+    generateColors();
     
 
     
-
-
 
 
 

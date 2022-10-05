e372ad3aa98b2c1cbc77d361521b694c6d86fbc2
Allan Medeiros Minato <allan.minato@gmail.com>
2022-10-05 17:58:46 -0300

    resolução exercicio 8,9 e TALVEZ o 10

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
+ 
     let color = document.getElementsByClassName('color')[0];
     let color2 = document.getElementsByClassName('color')[1];
     let color3 = document.getElementsByClassName('color')[2];
     let color4 = document.getElementsByClassName('color')[3];
-
+   
 
     color.style.backgroundColor = "black";
     color2.style.backgroundColor = "pink";
@@ -10,6 +11,8 @@
     color4.style.backgroundColor = "brown";
 
     const colorPalette = document.querySelectorAll(".color");
+
+    
 
     function generateColors() {
         colorPalette.forEach((color) =>{
@@ -28,14 +31,12 @@
     blackColor.classList = 'color selected';
     }
 
-
     const container = document.querySelector('#pixel-board');
     const sizeE1 = document.querySelector('.size');
     const size = sizeE1.value;
     const colorInside = document.querySelector('.color'); 
     const reset = document.querySelector('#clear-board')
 
-    let draw = false;
         function populate(size) {
             container.style.setProperty('--size', size)
             
@@ -45,35 +46,46 @@
                 div.classList.add('pixel')
                 container.appendChild(div)
                 reset.addEventListener("click", function(){
-                    if(div.style.backgroundColor != 'rgb(255, 255, 255)'){
-                        div.style.backgroundColor = 'rgb(255, 255, 255)'
-                    }
+                if(div.style.backgroundColor != 'rgb(255, 255, 255)'){
+                div.style.backgroundColor = 'rgb(255, 255, 255)'
+                }               
                 
-                      
-            }
-                
-            
+               
+                   
+                }
             )}
-               
-               
-        
             }
 
-        populate(5)
-
-        window.addEventListener("mousedown", function(){
-            draw = false
-        })
-
-        window.addEventListener("mouseup", function(){
-            draw = true
-        })
-
-     
     generateColors();
-   
+    populate(5);
 
 
+    let colorPaletteSelected = document.getElementById('color-palette');
+    colorPaletteSelected.addEventListener("click", function(select) {
+      if (select.target.className == 'color'){ 
+        let colorSelect = document.querySelector(".selected");  
+        colorSelect.classList.remove("selected");
+        select.target.classList.add("selected");
+        }
+      }
+    )
+    
+
+    
+    container.addEventListener('click', function(paintPixel) {
+        let selectedColor = document.querySelector('.selected');
+        let paint = getComputedStyle(selectedColor).backgroundColor;
+        let paintedPixel = paintPixel.target;
+        paintedPixel.style.backgroundColor = paint;
+      }
+      )
+
+    
+
+
+// pixel.addEventListener('click', function(event){
+//     pixel.style.backgroundColor = color2.style.backgroundColor;
+// })
     
 
 
